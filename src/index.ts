0e2166ac750fffdb031627e7208abe4e28110bc6
mikebuilds <michael@mjth.org>
2024-02-16 23:44:32 +1100

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+console.log("eventjuice is in alpha testing..");
+
+class eventjuice {
+  private key = undefined as string | undefined;
+
+  constructor(key:string) {
+    this.key = key;
+  }
+
+  async getUpcomingEvents() {
+    alert("eventjuice is in alpha testing..");
+  }
+}
+
+export default eventjuice;
